333f3e71448f977f29a322b30ccc5ca610bc60b5
djbeefburger <beefburger@gmail.com>
2016-12-26 12:42:28 -0500

    Create tiles.php

---
+++ tiles.php
@@ -0,0 +1,19 @@
+<?php
+
+class Tiles{
+  //type (social media, song, mix, etc)
+  //image (png)
+  //image watermark (png)
+  //url (rel?)
+  //target (if not local url,_blank)
+  //text (html)
+  //index (int)
+  //position (float)
+  
+  private function wrapHtml($str){
+    
+  }
+
+}
+
+?>
