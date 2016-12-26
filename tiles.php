e446c569a6e2ad7d7a2f58bd880c16aeaa9b0b35
djbeefburger <beefburger@gmail.com>
2016-12-26 13:40:30 -0500

    Update tiles.php

--- tiles.php
+++ tiles.php
@@ -9,17 +9,30 @@ class Tiles{
   //text (html)
   //index (int)
   //position (float)
+  private function 
   
-  private function tagHtml(&$html,$tag){
-    $html="<$tag>$html</$tag>";
+  private function addHtmlTtag(&$html_str,$tag_str,$attribute_array=array()){
+    if(empty($html)) $html="</$tag>";
+    elseif(empty($attribute))$html="<$tag>$html</$tag>";
+    elseif(is_array($attribute_mixed))
+    else $html="<$tag
     return $html;
   }
   
-  private function setId(&$html,$id){
-    
+  
+  private function formatHtmlAttribute($attribute,$value){
+    return " $attribute" . '="' . $value . '"';
   }
   
-  private function setClass(&$html,$id){
+  private function addHtmAttribute(&$html,$attribute,$value){
+    $position=strpos($html,">");
+    $part1=substr($html,0,$position-1);
+    $part2=substr($html,$position);
+    $html=$part1.$this->formatHtmlAttribute($attribute,$value).$part2;
+  }
+  
+  
+  private function addHtmlClass(&$html,$id){
     
   }
   
